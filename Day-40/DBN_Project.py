217fd8991dee1163224eedaebb2f12b6dd7664db
Chaitra-Bhat383 <79207846+Chaitra-Bhat383@users.noreply.github.com>
2021-11-20 15:36:18 +0530

    Update DBN_Project.py

--- Day-40/DBN_Project.py
+++ Day-40/DBN_Project.py
@@ -1,34 +1,31 @@
-import numpy as np
-
-np.random.seed(1337)  # for reproducibility
-from sklearn.datasets import load_digits
-from sklearn.model_selection import train_test_split
-from sklearn.metrics.classification import accuracy_score
-
 from dbn.tensorflow import SupervisedDBNClassification
+import numpy as np
+import pandas as pd
+from sklearn.model_selection import train_test_split
+from sklearn.metrics import accuracy_score
 
+digits = pd.read_csv("train.csv")
 
-# Loading dataset
-digits = load_digits()
-X, Y = digits.data, digits.target
+X = np.array(digits.drop(["label"], axis=1))
+Y = np.array(digits["label"])
 
-# Data scaling
-X = (X / 16).astype(np.float32)
+from sklearn.preprocessing import standardscaler
+ss=standardscaler()
+X = ss.fit_transform(X)
 
-# Splitting data
 X_train, X_test, Y_train, Y_test = train_test_split(X, Y, test_size=0.2, random_state=0)
 
-# Training
-classifier = SupervisedDBNClassification(hidden_layers_structure=[256, 256],
-                                         learning_rate_rbm=0.05,
-                                         learning_rate=0.1,
-                                         n_epochs_rbm=10,
-                                         n_iter_backprop=100,
-                                         batch_size=32,
-                                         activation_function='relu',
-                                         dropout_p=0.2)
+classifier = SupervisedDBNClassification(hidden_layers_structure =      
+[256, 256],
+learning_rate_rbm=0.05,
+learning_rate=0.1,
+n_epochs_rbm=10,
+n_iter_backprop=100,
+batch_size=32,
+activation_function='relu',
+dropout_p=0.2)
+
 classifier.fit(X_train, Y_train)
 
-# Test
 Y_pred = classifier.predict(X_test)
 print('Done.\nAccuracy: %f' % accuracy_score(Y_test, Y_pred))
